98e3543b78e9dc415d0ebcc3dd8d6a669ad980ad
SerhiiKitral <serhiy.kitral@gmail.com>
2024-10-05 20:42:32 +0300

    updated

--- main.py
+++ main.py
@@ -10,21 +10,33 @@ def find_trams_at_stop(stop_name, tram_routes):
             if stop_name in stops:
                 trams.append(tram)
     if trams:
-        return f"Трамваї, які проходять через зупинку '{stop_name}': {', '.join(list(set(trams)))}"
+        return f"Трамваї, які проходять через зупинку '{stop_name}': {', '.join(sorted(list(set(trams))))}"
     return f"Зупинка '{stop_name}' не знайдена в жодному маршруті."
 
 
 # Function 2: Calculate number of stops and transfers between two stops
 def calculate_stops_and_transfers(start_stop, end_stop, tram_routes):
-    queue = collections.deque([(start_stop, 0, 0, None)])  # (current stop, transfers, stops count, current tram)
+    if start_stop == end_stop:
+        return 'Початкова і кінцева зупинки однакові.'
+    queue = collections.deque([(start_stop, 0, None, [])])  # stop name, transfers, current_tram, path
     visited = set()
 
     while queue:
-        current_stop, transfers, stop_count, current_tram = queue.popleft()
+        current_stop, transfers, current_tram, path = queue.popleft()
 
         if current_stop == end_stop:
-            return f"Кількість зупинок: {stop_count}, кількість пересадок: {transfers}"
+            total_transfers = transfers - 1
+            total_stops = 0
+            current_stop_index = tram_routes[path[0].split(',')[0]][path[0].split(',')[1]].index(start_stop)
+            for item in path:
+                tram_number = item.split(',')[0]
+                direction = item.split(',')[1]
+                next_stop = item.split(',')[2]
+                total_stops += tram_routes[tram_number][direction].index(next_stop) - current_stop_index
+                current_stop_index = tram_routes[tram_number][direction].index(next_stop)
+            return f"Кількість зупинок: {total_stops}, кількість пересадок: {total_transfers}"
 
+        # Prevent visiting the same stop multiple times
         if current_stop in visited:
             continue
         visited.add(current_stop)
@@ -34,20 +46,25 @@ def calculate_stops_and_transfers(start_stop, end_stop, tram_routes):
                 if current_stop in stops:
                     stop_index = stops.index(current_stop)
 
-                    # Forward direction
+                    # Check for forward direction
                     for i in range(stop_index + 1, len(stops)):
                         next_stop = stops[i]
                         new_transfers = transfers if current_tram == tram else transfers + 1
-                        queue.append((next_stop, new_transfers, stop_count + 1, tram))
+                        queue.append(
+                            (next_stop, new_transfers, tram, path +
+                             [
+                                 f'{tram},{direction},{next_stop}']))
 
-                    # Backward direction
+                    # Check for backward direction
                     for i in range(stop_index - 1, -1, -1):
                         next_stop = stops[i]
                         new_transfers = transfers if current_tram == tram else transfers + 1
-                        queue.append((next_stop, new_transfers, stop_count + 1, tram))
+                        queue.append(
+                            (next_stop, new_transfers, tram, path +
+                             [
+                                 f'{tram},{direction},{next_stop}']))
 
-    return f"Маршруту між зупинками '{start_stop}' та '{end_stop}' не існує."
-
+    return "Маршруту не існує"
 
 # Function 3: Find trams going through all stops in *args
 def find_trams_through_all_stops(*stops, tram_routes):
@@ -77,6 +94,8 @@ def find_trams_through_all_stops(*stops, tram_routes):
 
 # Function to find routes between stops (existing from previous code)
 def find_routes_between_stops(start_stop, end_stop, tram_routes):
+    if start_stop == end_stop:
+        return 'Початкова і кінцева зупинки однакові.'
     queue = collections.deque([(start_stop, 0, None, [])])  # stop name, transfers, current_tram, path
     visited = set()
 
@@ -84,7 +103,7 @@ def find_routes_between_stops(start_stop, end_stop, tram_routes):
         current_stop, transfers, current_tram, path = queue.popleft()
 
         if current_stop == end_stop:
-            return f"Потрібний шлях:\n{'\n'.join(path)}\nПотрібна кількість пересадок: {transfers}"
+            return f"Потрібний шлях:\n{'\n'.join(path)}\nПотрібна кількість пересадок: {transfers - 1}"
 
         # Prevent visiting the same stop multiple times
         if current_stop in visited:
@@ -122,6 +141,7 @@ def main():
         tram_stops = json.load(file)
 
     print(find_trams_at_stop("Залізничний вокзал", tram_stops))
+    print(find_routes_between_stops("Залізничний вокзал", "вул. Коломийська", tram_stops))
     print(calculate_stops_and_transfers("Залізничний вокзал", "вул. Коломийська", tram_stops))
     print(find_trams_through_all_stops("Залізничний вокзал", "пл. Франка", "вул. Коломийська", tram_routes=tram_stops))
 
